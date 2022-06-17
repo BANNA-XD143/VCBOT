decf80e733aa4177951ceb2d731843b8ec58818c
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-06-17 15:31:17 +0530

    Update admins.py

--- AaruMusic/helpers/admins.py
+++ AaruMusic/helpers/admins.py
@@ -2,8 +2,8 @@ from typing import List
 
 from pyrogram.types import Chat
 
-from KaalMusic.helpers.get_admins import get as gett
-from KaalMusic.helpers.get_admins import set
+from AARUMusic.helpers.get_admins import get as gett
+from AARUMusic.helpers.get_admins import set
 
 
 async def get_administrators(chat: Chat) -> List[int]:
