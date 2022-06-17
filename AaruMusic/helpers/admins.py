0a7c4b5ba9c4a6ea2e36defce5e16e0da7e2fee4
BANNA_XD <105870422+BANNA-XD143@users.noreply.github.com>
2022-06-17 15:10:14 +0530

    Add files via upload

--- AaruMusic/helpers/admins.py
+++ AaruMusic/helpers/admins.py
@@ -2,8 +2,8 @@ from typing import List
 
 from pyrogram.types import Chat
 
-from AaruMusic.helpers.get_admins import get as gett
-from AaruMusic.helpers.get_admins import set
+from KaalMusic.helpers.get_admins import get as gett
+from KaalMusic.helpers.get_admins import set
 
 
 async def get_administrators(chat: Chat) -> List[int]:
